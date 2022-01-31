c212a72db38404803fb8f5ec606638c9d81c5db6
mroz <mvarmuza@gmail.com>
2022-01-31 22:45:19 +0100

    wip

--- packages/suite-data/scripts/list-unused.ts
+++ packages/suite-data/scripts/list-unused.ts
@@ -3,25 +3,14 @@ const { spawnSync } = require('child_process');
 
 import messages from '../../suite/src/support/messages';
 
-const unused: string[] = [];
-
+console.log('unused messages: ');
 for (const message in messages) {
-    console.log('checking: ', message);
     const { stdout } = spawnSync('bash', ['suite-data/scripts/find-unused.sh', message], {
         encoding: 'utf-8',
         cwd: '../',
     });
 
     if (!stdout) {
-        unused.push(message);
-        console.log('miss');
-    } else {
-        console.log('ok');
+        console.log(message);
     }
-}
-
-if (unused.length) {
-    console.log('not used:');
-    console.log(unused);
-    process.exit(1);
 }
